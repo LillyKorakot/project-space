43ec37031034f4578c53c6a5ec571d0a4f3bbede
Ly <korakot.jarunongkran>
2023-08-15 19:23:14 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
