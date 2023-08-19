05f89fa7f00d97cab3eba9f168abc65fcd1941bb
Michael <sitthisaklo@kkumail.com>
2023-08-19 14:07:16 +0700

    feat:fixed-gap-between-background

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,10 +4,12 @@ import Bitmap from "../assets/Bitmap.png";
 
 const Home = () => {
   return (
-    <div className="flex relative p-0 m-0 box-border  ">
+    <div
+      className="flex relative p-0 m-0 box-border  "
+      style={{ backgroundImage: `url(${spacehome})` }}
+    >
       <div
-        className=" bg-cover bg-center  w-[486px] h-[screen] rotate-0 relative " //mr-10
-        style={{ backgroundImage: `url(${spacehome})` }}
+        className=" bg-cover bg-center w-[482px] h-[screen] rotate-0 relative  max-md:rotate-180 " //mr-10
       >
         {/* Left side content */}
         <div className="h-full flex items-center justify-center absolute right-[-200px] top-[50px] w-[500px] ">
@@ -20,7 +22,7 @@ const Home = () => {
                 SPACE
               </h1>
             </div>
-            <p className="text-[#8892b0] pt-3 max-w-[700px] border-">
+            <p className="text-[#8892b0] pt-3 max-w-[700px] ">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
@@ -31,7 +33,7 @@ const Home = () => {
       </div>
 
       <div
-        className=" h-screen bg-cover bg-center w-[75%] ml-0 pl-0 " //ml=10
+        className=" h-screen bg-cover bg-center w-[958px] ml-0 pl-0   "
         style={{ backgroundImage: `url(${Bitmap})` }}
       >
         {/* Right side content */}
