c0dc50412e4989ea2fd344c5ba7bef59d714d6af
Michael <sitthisaklo@kkumail.com>
2023-08-27 11:08:34 +0700

    feat:assets-export-byname

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import crew_desktop from "./crew/background-crew_desktop.jpg";
+import crew_mobile from "./crew/background-crew_mobile.jpg";
+import crew_tablet from "./crew/background-crew-tablet.jpg";
+im;
