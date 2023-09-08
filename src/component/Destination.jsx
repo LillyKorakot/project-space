3425c993fba95768749fea48cb3ccfb9b88ed33f
Ly <korakot.jarunongkran>
2023-09-09 00:43:07 +0700

    feat: change landing page

--- src/component/Destination.jsx
+++ src/component/Destination.jsx
@@ -4,69 +4,36 @@ import { destination_mobile } from "../assets";
 import { moon } from "../assets";
 
 const Destination = () => {
-
-
-  const backgroundImage = {
-    backgroundImage: `url(${destination_desktop})`,
-    height: "100vh",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    
-  };
-
   return (
-    <div className=" w-full h-full flex items-center " style={backgroundImage}>
-      <div className=" max-w-[1500px] mx-auto px-8 flex flex-col justify-center">
-        <div className=" px-4 flex-col gap-8 my-20">
-          <div className="text-white font-barlow text-[28px]font-normal font-400 leading-normal tracking-[4.725px] uppercase ml-10 ">
-            <span className=" opacity-40">01 </span>
-            Pick your destination
+    <div className="flex flex-col justify-center items-center w-full h-[1000px] bg-black m-0 p-0">
+      <div className="text-white font-barlow text-[28px] font-normal font-400 leading-normal tracking-[4.725px] uppercase w-[50%] mb-20">
+        <span className="opacity-40">01 </span>
+        Pick your destination
+      </div>
+      {/* box */}
+      <div className="flex flex-col md:flex-row justify-center w-[50%] gap-32">
+        <div className="flex items-center text-white">
+          <div className="flex items-start">
+            <img src={moon} alt="moon" />
           </div>
-          <div></div>
         </div>
-        <div className="  w-full grid sm:grid-cols-2 gap-28">
-          <div className="flex items-center justify-center">
-            <img src={moon} alt="Moon" className="max-w-full h-auto" />
+        <div className="flex flex-col items-start justify-center w-full">
+          <div>
+            <ul className="flex text-[#D0D6F9] font-barlow text-[16px] font-normal tracking-[2.7px] ">
+              <li className="mx-2">MOON</li>
+              <li className="mx-2">MARS</li>
+              <li className="mx-2">EUROPA</li>
+              <li className="mx-2">TITAN</li>
+            </ul>
           </div>
-          <div className=" w-[445px] h-[472px] flex flex-col justify-center">
-            <div>
-              <ul className=" flex  w-[285.5px] h-[34px] text-[#D0D6F9] font-barlow text-[16px] font-normal tracking-[2.7px] ">
-                <li className=" mx-2">MOON</li>
-                <li className=" mx-2">MARS</li>
-                <li className=" mx-2">EUROPA</li>
-                <li className=" mx-2">TITAN</li>
-              </ul>
-            </div>
-
-            <h1 className=" text-white font-belle text-[100px] font-normal ">
-              MOON
-            </h1>
-            <p className=" text-[#D0D6F9] font-barlow text-[18px] font-normal font-400 leading-[32px]">
-              See our planet as you’ve never seen it before. A perfect relaxing
-              trip away to help regain perspective and come <br />
-              back refreshed. While you’re there, take in some history <br />
-              by visiting the Luna 2 and Apollo 11 landing sites.
-            </p>
-            <hr className=" pt-10 mt-[54px] mb-[10px] border-[#383B4B] " />
-            <div className=" grid grid-cols-2">
-              <div>
-                <h1 className=" text-[#D0D6F9] font-barlow text-[14px] font-normal tracking-[2.362px] uppercase">
-                  AVG. DISTANCE
-                </h1>
-                <p className=" text-white font-belle text-[28px] font-normal uppercase">
-                  384,400 km
-                </p>
-              </div>
-              <div>
-                <h1 className=" text-[#D0D6F9] font-barlow text-[14px] font-normal tracking-[2.362px] uppercase">
-                  Est. travel time
-                </h1>
-                <p className=" text-white font-belle text-[28px] font-normal uppercase">
-                  3 days
-                </p>
-              </div>
-            </div>
-          </div>
+          <h1 className="text-white font-belle text-[100px]">MOON</h1>
+          <p className="text-[#D0D6F9] font-barlow text-[18px] font-normal font-400">
+            See our planet as you’ve never seen it before. A perfect relaxing
+            trip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
+          <div></div>
         </div>
       </div>
     </div>
