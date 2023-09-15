225e7768f2f74e3101df5cd7a647e28c39e740eb
Lilly <korakot.jarunongkran@gmail.com>
2023-09-15 23:49:41 +0700

    feat: finished destination page

--- src/component/Destination.jsx
+++ src/component/Destination.jsx
@@ -1,39 +1,89 @@
-import React from "react";
-import { destination_desktop } from "../assets";
-import { destination_mobile } from "../assets";
-import { moon } from "../assets";
+import React, { useState } from "react";
+import { destination_desktop, moon, mars, europa, titan } from "../assets";
+import destinations from "../data/destination.jsx";
 
 const Destination = () => {
+  const [selectedDestination, setSelectedDestination] = useState(
+    destinations[0]
+  );
+
+  const handleDestinationClick = (destinationName) => {
+    const selected = destinations.find(
+      (destination) => destination.name === destinationName
+    );
+    if (selected) {
+      setSelectedDestination(selected);
+    }
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center w-full h-[1000px] bg-black m-0 p-0">
-      <div className="text-white font-barlow text-[28px] font-normal font-400 leading-normal tracking-[4.725px] uppercase w-[50%] mb-20">
-        <span className="opacity-40">01 </span>
-        Pick your destination
-      </div>
-      {/* box */}
-      <div className="flex flex-col md:flex-row justify-center w-[50%] gap-32">
-        <div className="flex items-center text-white">
-          <div className="flex items-start">
-            <img src={moon} alt="moon" />
+    <div
+      className="w-full h-screen flex items-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${destination_desktop})` }}
+    >
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="my-20">
+          <div className="text-white font-barlow text-[28px] md:text-xl lg:text-[28px] leading-normal tracking-widest ml-10">
+            <span className="opacity-40">01 </span>
+            PICK YOUR DESTINATION
           </div>
         </div>
-        <div className="flex flex-col items-start justify-center w-full">
-          <div>
-            <ul className="flex text-[#D0D6F9] font-barlow text-[16px] font-normal tracking-[2.7px] ">
-              <li className="mx-2">MOON</li>
-              <li className="mx-2">MARS</li>
-              <li className="mx-2">EUROPA</li>
-              <li className="mx-2">TITAN</li>
-            </ul>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md: ">
+          <div className="flex items-center justify-center hover:animate-spin ">
+            {selectedDestination.name === "MOON" && (
+              <img src={moon} alt="Moon" className="max-w-full h-auto" />
+            )}
+            {selectedDestination.name === "MARS" && (
+              <img src={mars} alt="Mars" className="max-w-full h-auto" />
+            )}
+            {selectedDestination.name === "EUROPAS" && (
+              <img src={europa} alt="Europa" className="max-w-full h-auto" />
+            )}
+            {selectedDestination.name === "TITAN" && (
+              <img src={titan} alt="Titan" className="max-w-full h-auto" />
+            )}
           </div>
-          <h1 className="text-white font-belle text-[100px]">MOON</h1>
-          <p className="text-[#D0D6F9] font-barlow text-[18px] font-normal font-400">
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
-          </p>
-          <div></div>
+          <div className="w-full md:w-3/5 h-auto flex flex-col justify-center">
+            <div>
+              <ul className="flex space-x-4 text-[#D0D6F9] font-barlow text-[16px] sm:text-[16px] lg:text-[16px]">
+                {destinations.map((destination) => (
+                  <li
+                    key={destination.id}
+                    className={`cursor-pointer ${
+                      selectedDestination.name === destination.name
+                        ? "text-white font-bold underline underline-offset-[12px]"
+                        : "hover:text-white"
+                    }`}
+                    onClick={() => handleDestinationClick(destination.name)}
+                  >
+                    {destination.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <h1 className="text-white font-belle text-5xl md:text-[100px] lg:text-[100px] pt-9 pb-4">
+              {selectedDestination.name}
+            </h1>
+            <p className=" w-[450px] text-[#D0D6F9] font-barlow text-[18px] leading-[32px] ">
+              {selectedDestination.description}
+            </p>
+            <hr className="my-5 border-[#383B4B] border-t-2" />
+            <div className="grid grid-cols-2 gap-[79px] text-[#D0D6F9] font-barlow">
+              <div>
+                <h1 className="uppercase text-sm md:text-[14px] lg:text-[14px] pb-3 ">AVG. DISTANCE</h1>
+                <p className=" uppercase text-white font-belle text-sm md:text-[28px] lg:text-[28px]">
+                  {selectedDestination.distance}
+                </p>
+              </div>
+              <div>
+                <h1 className="uppercase text-sm md:text-[14px] lg:text-[14px] pb-3 ">Est. travel time</h1>
+                <p className=" uppercase  text-white font-belle text-sm md:text-[28px] lg:text-[28px]">
+                  {selectedDestination.travel}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
