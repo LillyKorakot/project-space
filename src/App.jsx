88f1a6691bbd45cdd4c01f7531784831d494a8f0
Lilly <korakot.jarunongkran@gmail.com>
2023-09-17 02:06:15 +0700

    feat: finished technology page with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
     <Navbar/>
     <Home/>
-    {/* <Destination/> */}
+    <Destination/>
     <Crew/>
     <Technology/>
     </>
--- src/data/technology.jsx
+++ src/data/technology.jsx
@@ -9,7 +9,6 @@ const technology = [
   {
     id: "2",
     name: "Spaceport",
-
     description:
       "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
   },
